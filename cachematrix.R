71205aa4ea134320dcb6359afdfa7792282fa25d
Bin Tang <bin@identitymind.com>
2015-09-27 16:26:42 -0700

    checkin

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(iv) m <<- iv
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
@@ -12,4 +20,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  m <- makeCacheMatrix(x)$getinverse()
+  if (!is.null(m)) {
+    return(m)
+  }
+  m <- solve(x)
+  makeCacheMatrix(x)$setinverse(m)
+  m
 }
